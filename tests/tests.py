f431ea7440f986dc76e6566c039a561f0d90858f
Ronny Rentner <github@ronny-rentner.de>
2022-06-18 10:22:02 +0200

    Attempt to analyse test errors

--- tests/tests.py
+++ tests/tests.py
@@ -20,13 +20,13 @@ class ultraimportTests(unittest.TestCase):
     def test_example_myprogram(self):
         filename = "examples/working/myprogram/run.py"
         ret = self.exec(filename)
-        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error.')
+        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error: {ret}')
         self.assertEqual(ret.stdout, b"""I did something\ncache store: I did something\ncache store: Something\n""")
 
     def test_example_mypackage(self):
         filename = "examples/working/mypackage/run.py"
         ret = self.exec(filename)
-        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error.')
+        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error: {ret}')
         self.assertEqual(ret.stdout, b"submodule start  submodule\n"
             b"hello from xmodule.py\n"
             b"hello from siblingmodule.py\n"
@@ -38,7 +38,7 @@ class ultraimportTests(unittest.TestCase):
     def test_example_dependency_injection(self):
         filename = "examples/working/dependency-injection/run.py"
         ret = self.exec(filename)
-        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error.')
+        self.assertEqual(ret.returncode, 0, f'Running {filename} did return with an error: {ret}')
         self.assertEqual(ret.stdout, b"injected logger: worker.py is doing some work\n")
 
 if __name__ == '__main__':
