35927378f014a82ad142b3807a82fcc69b3d7c31
Ronny Rentner <github@ronny-rentner.de>
2022-06-18 10:58:00 +0200

    Fix Python version support

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
     zip_safe=False,
     ext_modules=Cython.Build.cythonize(ext, compiler_directives={'language_level' : "3"}),
     setup_requires=['cython>=0.24.1'],
-    python_requires=">=3.8",
+    python_requires=">=3.5",
 )
