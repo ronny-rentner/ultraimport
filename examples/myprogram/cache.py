35146590aad388d3772af1268aa894320b7a9d1a
Ronny Rentner <github@ronny-rentner.de>
2022-11-21 07:58:56 +0100

    Improve error reporting when using lazy loading

--- examples/myprogram/cache.py
+++ examples/myprogram/cache.py
@@ -1,14 +1,16 @@
 import ultraimport
 
-# Lazy load the logger by specifying the objects_to_import (2nd parameter)
+# Lazy load the logger by specifying the `objects_to_import` (2nd parameter)
 # as a dict with the type as the value. This is necessary to resolve the cyclic
 # dependency because the cache is using the logger and the logger is using the cache.
-logger = ultraimport('__dir__/log.py', {'logger': callable}, lazy=True)
+logger = ultraimport('__dir__/logging.py', {'logger': callable}, lazy=True)
 
 data = []
 
 def store(value):
     if not value.startswith('cache'):
+        # Only when we actually try to log something through the logger() function,
+        # the respective code in logging.py is actually loaded.
         logger('cache store: ' + value)
     data.append(value)
 
